50e184f386d0a178fb92f2810be44b4e72f7eee9
Gen <genesishawkeye42@gmail.com>
2023-01-23 17:55:25 -0300

    fixed the fetchMoedas method

--- src/app/services/exchangerate.service.ts
+++ src/app/services/exchangerate.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
+import { IMoeda } from '../interfaces/Moeda';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IData } from '../interfaces/exchangeServiceData';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExchangerateService {
-  private coins = [];
+  private symbolsURL = 'https://api.exchangerate.host/symbols';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  public async getSymbos() {
-    const res = await fetch('https://api.exchangerate.host/symbols');
-    const data = await res.json();
-
-    console.log(data.symbols);
+  fetchMoedas(): Observable<any> {
+    return this.http.get(this.symbolsURL);
   }
 }
