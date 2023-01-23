d42ca175c72e057e823761a49b6b013df5823a40
Gen <genesishawkeye42@gmail.com>
2023-01-23 17:54:34 -0300

    added http client import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
@@ -38,6 +39,7 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatPaginatorModule,
     MatInputModule,
     MatSortModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
