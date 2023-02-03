f4cf09e616ae8d2722dba4677a38488c0e995753
Gen <genesishawkeye42@gmail.com>
2023-02-02 21:04:57 -0300

    added confirmar exclusão component

---
+++ src/app/components/confirmar-exclusao/confirmar-exclusao.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmar-exclusao',
+  templateUrl: './confirmar-exclusao.component.html',
+  styleUrls: ['./confirmar-exclusao.component.css'],
+})
+export class ConfirmarExclusaoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
