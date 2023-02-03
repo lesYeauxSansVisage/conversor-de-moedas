063a1ddc3fff2a4ac17142e94ba210adfccc2ecb
Gen <genesishawkeye42@gmail.com>
2023-02-02 21:05:28 -0300

    added excluir items and formatar dados method

--- src/app/services/historico.service.ts
+++ src/app/services/historico.service.ts
@@ -1,21 +1,61 @@
 import { Injectable } from '@angular/core';
 import { IHistoricoItem } from '../interfaces/IHistoricoItem';
+import { EventEmitter } from '@angular/core';
+import { IConversaoData } from '../interfaces/IConversaoData';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HistoricoService {
-  items: IHistoricoItem[];
+  alteracaoHistorico = new EventEmitter<IHistoricoItem[]>();
 
-  constructor() {}
+  items: IHistoricoItem[] = [];
 
-  public getItemsSessao() {
+  constructor() {
     if (sessionStorage.getItem('items')) {
-      console.log('Há items salvos!');
+      const items: string = sessionStorage.getItem('items')!;
+      this.items = JSON.parse(items) as IHistoricoItem[];
     }
+  }
+
+  getItems() {
+    return this.items;
   }
 
   salvarItemsSessao() {
     sessionStorage.setItem('items', JSON.stringify(this.items));
+  }
+
+  adicionarItem(item: IHistoricoItem) {
+    this.items.push(item);
+    this.alteracaoHistorico.emit(this.items);
+    this.salvarItemsSessao();
+  }
+
+  excluirItem(id: number) {
+    this.items = this.items.filter((item) => item.id !== id);
+    this.alteracaoHistorico.emit(this.items);
+    this.salvarItemsSessao();
+  }
+
+  formatarDados(data: IConversaoData, valor: number): IHistoricoItem {
+    const dataAtual: Date = new Date();
+
+    const itemsLength = this.getItems().length;
+
+    const valorAlto = valor > 10000;
+
+    const id = itemsLength > 0 ? this.getItems()[itemsLength - 1].id! + 1 : 1;
+
+    return {
+      id: id,
+      data: dataAtual,
+      moedaDeDestino: data.query.to,
+      moedaDeOrigem: data.query.from,
+      valorDeEntrada: data.query.amount,
+      valorDeSaida: data.result,
+      taxa: data.info.rate,
+      valorAlto: valorAlto,
+    };
   }
 }
