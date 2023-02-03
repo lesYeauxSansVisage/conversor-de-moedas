33cc3a00ced9bf0f2af55b41de1c27cc830a6b07
Gen <genesishawkeye42@gmail.com>
2023-02-03 20:04:21 -0300

    added tests for exhangerate service

--- src/app/services/exchangerate.service.spec.ts
+++ src/app/services/exchangerate.service.spec.ts
@@ -1,16 +1,68 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { ExchangerateService } from './exchangerate.service';
+import { ISimbolosData } from '../interfaces/ISimbolosData';
+import { IConversaoData } from '../interfaces/IConversaoData';
 
 describe('ExchangerateService', () => {
   let service: ExchangerateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ExchangerateService],
+    });
     service = TestBed.inject(ExchangerateService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('fetchMoedas', () => {
+    it('deveria retornar uma resposta com um objeto com todas as moedas disponíveis', () => {
+      const resposta: ISimbolosData = {
+        motd: 'Aqui está sua resposta',
+        sucess: true,
+        symbols: {},
+      };
+
+      let response: ISimbolosData;
+
+      spyOn(service, 'fetchMoedas').and.returnValue(of(resposta));
+
+      service.fetchMoedas().subscribe((res: ISimbolosData) => {
+        response = res;
+
+        expect(response).toEqual(resposta);
+      });
+    });
+  });
+
+  describe('converterMoeda', () => {
+    it('deveria retornar um objeto contendo dados da conversão', () => {
+      const userResponse: IConversaoData = {
+        query: {
+          from: 'USD',
+          to: 'BRL',
+          amount: 10,
+        },
+        result: 9.179222,
+        date: '2023-02-03',
+        info: {
+          rate: 0.917922,
+        },
+      };
+
+      let response: IConversaoData;
+
+      spyOn(service, 'converterMoeda').and.returnValue(of(userResponse));
+
+      service
+        .converterMoeda('USD', 'BRL', 10)
+        .subscribe((res: IConversaoData) => {
+          response = res;
+
+          expect(response).toEqual(userResponse);
+        });
+    });
   });
 });
