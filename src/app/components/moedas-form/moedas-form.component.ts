28d2e29101e99b6e19d40ae5019feca608aab845
Gen <genesishawkeye42@gmail.com>
2023-02-05 17:08:00 -0300

    added snackbar message in case of error

--- src/app/components/moedas-form/moedas-form.component.ts
+++ src/app/components/moedas-form/moedas-form.component.ts
@@ -3,6 +3,11 @@ import { ExchangerateService } from 'src/app/services/exchangerate.service';
 import { IHistoricoItem } from 'src/app/interfaces/IHistoricoItem';
 import { IConversaoData } from 'src/app/interfaces/IConversaoData';
 import { HistoricoService } from 'src/app/services/historico.service';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 import { map, zip } from 'rxjs';
 
 @Component({
@@ -25,7 +30,8 @@ export class MoedasFormComponent implements OnInit {
 
   constructor(
     private exchangeRateService: ExchangerateService,
-    private historicoService: HistoricoService
+    private historicoService: HistoricoService,
+    private _snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -38,14 +44,22 @@ export class MoedasFormComponent implements OnInit {
     if (this.moedaOrigem == 'USD') {
       this.exchangeRateService
         .converterMoeda(this.moedaOrigem, this.moedaDestino, +this.valor)
-        .subscribe((data: IConversaoData) => {
-          this.conversaoAtual = this.historicoService.formatarDados(
-            data,
-            data.query.amount
-          );
+        .subscribe(
+          (data: IConversaoData) => {
+            this.conversaoAtual = this.historicoService.formatarDados(
+              data,
+              data.query.amount
+            );
 
-          this.atualizarConversaoAtual();
-        });
+            this.atualizarConversaoAtual();
+
+            this.openSnackBar('Sua conversão está disponível no histórico.');
+          },
+          (err) =>
+            this.openSnackBar(
+              'Houve um erro na conversão. Por favor tente novamente.'
+            )
+        );
     } else {
       zip(
         this.exchangeRateService.converterMoeda(
@@ -59,14 +73,21 @@ export class MoedasFormComponent implements OnInit {
         )
       )
         .pipe(map(([response1, response2]) => ({ response1, response2 })))
-        .subscribe((result) => {
-          this.conversaoAtual = this.historicoService.formatarDados(
-            result.response1,
-            (result.response2 as IConversaoData).result
-          );
+        .subscribe(
+          (result) => {
+            this.conversaoAtual = this.historicoService.formatarDados(
+              result.response1,
+              (result.response2 as IConversaoData).result
+            );
 
-          this.atualizarConversaoAtual();
-        });
+            this.atualizarConversaoAtual();
+            this.openSnackBar('Sua conversão está disponível no histórico.');
+          },
+          (err) =>
+            this.openSnackBar(
+              'Houve um erro na conversão. Por favor tente novamente.'
+            )
+        );
     }
 
     this.resetarForm();
@@ -82,5 +103,14 @@ export class MoedasFormComponent implements OnInit {
     this.historicoService.adicionarItem(this.conversaoAtual);
 
     this.emitirConversao.emit(this.conversaoAtual);
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, '', {
+      horizontalPosition: 'start',
+      verticalPosition: 'bottom',
+      duration: 3 * 1000,
+      panelClass: ['bg-dark', 'text-light'],
+    });
   }
 }
